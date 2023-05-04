208ec42c549aa395b0022762d369eeb4c5319e04
DEADHEAD652 <hamzajjmalik@gmail.com>
2023-05-04 08:55:55 +0500

    home page

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,12 +1,20 @@
 import React from "react";
-import banner from "../assets/tracel.jpg";
+import banner from "../assets/banner.jpg";
 import Search from "./SearchLocation";
+import guide from '../assets/guide.jpg'
+import security from '../assets/security.jpg'
+
+
 
 const Home = () => {
   return (
     <div className="justify-center content-center w-auto  bg-white-100">
       <div className="">
-        <img src={banner} alt="banner" className="w-full max-w-sm sm:max-w-full"/>
+        <img
+          src={banner}
+          alt="banner"
+          className="w-full max-w-sm sm:max-w-full"
+        />
       </div>
       <div>
         <Search />
@@ -14,18 +22,29 @@ const Home = () => {
       <div className="align-middle  text-center text-7xl italic">
         Our Services
       </div>
+
       <div className="flex items-center py-4">
-      <img src={""} alt="banner" className="w-1/4 my-14 mx-60 "/>
-      <h1 className="text-5xl font-bold ">Hire Guide</h1> <br />
-      <p className="text-5x "> Trip to Trip provides you with direct contact with the local guide of your favourite destination and hire them </p>
+        <img src={guide} alt="banner" className="w-1/4 my-14 mx-60 " />
+        <div className="justify-center ">
+        <h1 className="text-5xl font-bold ">Hire Guide</h1> <br />
+        <p className="text-5x ">
+          {" "}
+          Trip to Trip provides you with direct contact with the local guide of
+          your favourite destination and hire them{" "}
+        </p>
+        </div>
       </div>
-      <div className="flex items-center">
-      <h1 className="text-5xl my-14 mx-60 font-bold">Hire Security</h1>
-      <p className="text-5x "> Trip to Trip provides you to hire security for your trips </p>
+      <div className="flex items-center ">
+        <div className="justify-center ">
+        <h1 className="text-5xl my-14 mx-60 font-bold">Hire Security</h1>
+        <p className="text-5x ">
+          {" "}
+          Trip to Trip provides you to hire security for your trips{" "}
+        </p>
+        </div>
 
-      <img src={""} alt="banner" className="w-1/4 "/>
+        <img src={security} alt="banner" className="w-1/4 " />
       </div>
-
     </div>
   );
 };
