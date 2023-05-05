754e9da027a598f1ad5b308c210af7f2064e1d7a
DEADHEAD652 <hamzajjmalik@gmail.com>
2023-05-05 21:06:13 +0500

    tours page

--- client/src/Pages/Tours.jsx
+++ client/src/Pages/Tours.jsx
@@ -10,17 +10,26 @@ const Tours = (props) => {
     }
   
     return (
-      <div>
+        <div class="grid grid-cols-3 gap-4">
         {state.tours.map((tour) => (
-          <div key={tour._id}>
-            <p>Guide: {tour.guide.firstname}</p>
-            <p>Title : {tour.title}</p>
-            <p>Summary: {tour.summary} </p>
-            <p>Description : {tour.description}</p>
-            <p>Price : {tour.price}</p>
+          <div key={tour._id} class="bg-white rounded-lg shadow-lg p-4">
+            <div class="flex justify-center">
+              <img src={tour.guide.avatar} alt={tour.guide.firstname} class="rounded-full w-24 h-24 object-cover" />
+            </div>
+            <div class="mt-4">
+                <p class="font-bold text-xl">{tour.guide.firstname}</p>
+              <p class="font-bold text-xl">{tour.title}</p>
+              <p class="text-gray-500">{tour.summary}</p>
+              <p class="font-bold text-lg mt-4">${tour.price}</p>
+             <button class="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded">
+  Description
+</button>
+
+            </div>
           </div>
         ))}
       </div>
+      
     );
   };
   
